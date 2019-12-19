f6d20e617d86ae66c87075474f31bd2b9ba5c83c
istvand <51999453+istvand-9n7@users.noreply.github.com>
2019-12-19 00:06:26 +0000

    Created a list of relative section coordinates

--- 2019/3/p1.d
+++ 2019/3/p1.d
@@ -2,13 +2,56 @@
 
 import std.algorithm: map;
 import std.array;
+import std.conv;
 import std.stdio : File, writeln;
 import std.string;
+
+auto process_section(char[] section) {
+    int[][] coords = [];
+    auto repeat = section[1..$].to!int; 
+    switch (section[0]) {
+        case 'L':
+            foreach (r; 0..repeat+1) {
+                coords ~= [-r,0]; 
+            }
+            break;
+        case 'R':
+            foreach (r; 0..repeat+1) {
+                coords ~= [r,0]; 
+            }            
+            break;
+        case 'U':
+            foreach (r; 0..repeat+1) {
+                coords ~= [0,r]; 
+            }            
+            break;
+        case 'D':
+            foreach (r; 0..repeat+1) {
+                coords ~= [0,-r]; 
+            }            
+            break;
+        default:
+            // TODO throw exception (or do sthing else if we want nothrow)
+            break;
+    }
+    return coords;
+}
+
+auto coord_list(char[][] wire_reading) {
+    auto loc = [0,0];
+    foreach (instr; wire_reading) {
+        process_section(instr);
+    }
+
+}
 
 void main() {
     auto input_file = File("input.txt", "r");
     scope(exit) input_file.close();
     auto wire_readings = input_file.byLine.map!(a => a.split(","));
-    writeln(typeid(wire_readings)); // p1.main.MapResult!(__lambda1, ByLineImpl!(char, char)).MapResult // LOL
+    // writeln(typeid(wire_readings)); // p1.main.MapResult!(__lambda1, ByLineImpl!(char, char)).MapResult // LOL
+    foreach (wire_reading; wire_readings){
+        coord_list(wire_reading.array());
+    }
 
 }
