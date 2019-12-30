ca6b020c22fbc7e3b9ad99c0804b93eb4919430c
istvand <51999453+istvand-9n7@users.noreply.github.com>
2019-12-30 18:17:10 +0000

    Intersection calculation

--- 2019/3/p1.d
+++ 2019/3/p1.d
@@ -4,6 +4,7 @@ import std.algorithm: map;
 import std.array;
 import std.conv;
 import std.math: abs;
+import std.range: takeExactly;
 import std.stdio : File, writeln;
 import std.string;
 
@@ -27,31 +28,31 @@ auto manhattan(GP pt1, GP pt2) {
 auto section_offset(GP[] section, GP offset){
     // I'd like to see it as <array> + scalar = <array>
     // or maybe <array>.offset(scalar) = <array>
-    return section.map!(pt => add(pt, offset));
+    return section.map!(pt => add(pt, offset)).array;
 }
 
 auto process_section(const char[] section) {
-    int[][] coords = [];
+    GP[] coords = [];
     auto repeat = section[1..$].to!int; 
     switch (section[0]) {
         case 'L':
             foreach (r; 0..repeat+1) {
-                coords ~= [-r,0]; 
+                coords ~= GP(-r,0); 
             }
             break;
         case 'R':
             foreach (r; 0..repeat+1) {
-                coords ~= [r,0]; 
+                coords ~= GP(r,0); 
             }            
             break;
         case 'U':
             foreach (r; 0..repeat+1) {
-                coords ~= [0,r]; 
+                coords ~= GP(0,r); 
             }            
             break;
         case 'D':
             foreach (r; 0..repeat+1) {
-                coords ~= [0,-r]; 
+                coords ~= GP(0,-r); 
             }            
             break;
         default:
@@ -61,31 +62,68 @@ auto process_section(const char[] section) {
     return coords;
 }
 
-auto coord_list(const char[][] wire_reading) {
-    auto loc = [0,0];
+// alternatively: store only the segment endpoints and use a segment intersection algorithm
+auto wire_coords(const char[][] wire_reading) {
+    GP[] wire = [GP(0,0)];  // TODO efficiency: hashtable to emulate a set
     foreach (instr; wire_reading) {
-        process_section(instr);
+        auto section_coords = process_section(instr)[1..$];  // TODO eliminate duplicates better?
+        wire ~= section_coords.section_offset(wire[$-1]);
     }
+    //writeln("Wire: ", wire);
+    return wire;
+}
 
+auto wire_intersections(GP[] wire1, GP[] wire2) {  // TODO efficiency: hashtables
+    GP[] intersections = [];
+    // cannot use setops because the wire coordinates are not sorted
+    foreach (coord1; wire1) {
+        foreach (coord2; wire2) {
+            if (coord1 == coord2) {
+                intersections ~= coord1;
+            }
+        }
+    }
+    return intersections;
 }
 
 int process_readings(const char[][][] wire_readings) {
     auto result = 0;
+    GP[][] coords = [];
     foreach (wire_reading; wire_readings){
-        coord_list(wire_reading);
+        coords ~= wire_coords(wire_reading);
     }
+    auto intersections = wire_intersections(coords[0], coords[1]);
+    writeln("Intersections: ", intersections);
     return result;
 }
 
 void main() {
     auto input_file = File("input.txt", "r");
     scope(exit) input_file.close();
-    auto wire_readings = input_file.byLine.map!(a => a.split(","));
+    auto wire_readings = input_file.byLine.takeExactly(2).map!(a => a.split(","));
+    //writeln(wire_readings);
     // writeln(typeid(wire_readings)); // p1.main.MapResult!(__lambda1, ByLineImpl!(char, char)).MapResult // LOL
     writeln(process_readings(wire_readings.array));
 }
 
 
+unittest {
+    auto input = [
+        ["R5","D7","R4","U4"],
+        ["U3","R3","D7","L2","U5"]
+    ];
+    GP[][] coords = [];
+    foreach (wire_reading; input){
+        coords ~= wire_coords(wire_reading);
+    }
+    auto expected = [GP(0,0), GP(1,0), GP(3,0)];
+    auto result = wire_intersections(coords[0], coords[1]);
+    writeln(result);
+    assert(result == expected);
+
+}
+
+// Test cases by AoC
 unittest {
     auto input = [
         ["R75","D30","R83","U83","L12","D49","R71","U7","L72"],
