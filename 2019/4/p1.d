70cada7c08b19fe29cdac10e6952712e313beb47
istvand <51999453+istvand-9n7@users.noreply.github.com>
2020-01-12 21:10:28 +0000

    [D4P1] Created a struct for the same thing
    A little encapsulation. Seems like some care needs to be taken with op. overloading and copy constructors.

--- 2019/4/p1.d
+++ 2019/4/p1.d
@@ -7,6 +7,67 @@ import std.typecons: Tuple, tuple;
 
 alias pw = int[6];
 
+struct Password6 {
+
+    pw _pw;
+
+    this(in pw pw) {
+        // TODO validate digit range
+        _pw = pw.dup;
+    }
+
+    this(int pw) {
+        // TODO validate length
+        _pw = int_to_pw(pw);
+        writeln(_pw);
+    }
+
+    this(ref return scope Password6 rhs) {
+        this._pw = rhs._pw.dup;
+    }
+
+    void _carry() {
+        auto carry = false;
+        if (_pw[$-1] == 10) {
+            _pw[$-1] = 0;
+            carry = true;
+        }
+        foreach_reverse (idx, digit; _pw[0..$-1]) {
+            if (carry) {
+                _pw[idx] += 1;
+                carry = false;
+            }
+            if (_pw[idx] == 10) {
+                _pw[idx] = 0;
+                carry = true;
+            }
+        }
+    }
+
+    // TODO functional design (return Password6)?
+    void incr() {
+        this._pw[$-1] += 1;
+        this._carry();
+    }
+
+    // TODO opAdd for int
+
+    bool valid() {
+        // dunno how to select the external function in case of a name clash
+        return check_valid(this._pw);
+    }
+
+    int opCmp(ref const Password6 rhs) const {
+        // TODO think about private _pw-s
+        // this._pw.opCmp(rhs._pw)  // did not work because undefidedness for parameter const(int[6])- how to forward then?
+        if (this._pw < rhs._pw) return -1;
+        if (this._pw > rhs._pw) return 1;
+        if (this._pw == rhs._pw) return 0;
+        return 0;
+    }
+
+}
+
 bool check_increase(pw pw) {
     int prev = pw[0];
     foreach (digit; pw[1..$]) {
@@ -29,10 +90,9 @@ bool check_dup(pw pw) {
     return false;
 }
 
-bool check_valid(pw pw) {
+bool check_valid(in pw pw) {
     return check_increase(pw) && check_dup(pw);
 }
-
 
 pw[] naive_generate(pw low, pw high) {
     pw current = low.dup;
@@ -72,18 +132,35 @@ pw int_to_pw(int num) {
     return digits;
 }
 
-int[] naive_generate(int low, int high) {
-    int[] result = [];
-    while (low < high) {
-        auto digits = int_to_pw(low);
-        if (check_valid(digits)) {
-            writeln(low);
-            result ~= low;
+Password6[] naive_generate(int low, int high) {
+    auto low_pw = Password6(low);
+    auto high_pw = Password6(high);
+    auto current = low_pw;
+    // assert(current !is low_pw);  // how to test for identity?
+    Password6[] result = [];
+    while (current < high_pw) {
+        if (current.valid()) {
+            //writeln(current);
+            result ~= current;
         }
-        low++;
+        current.incr();
     }
     return result;
 }
+
+// NOTE can't have 2 functions with the same params (return value isn't checked for unification)
+// int[] naive_generate(int low, int high) {
+//     int[] result = [];
+//     while (low < high) {
+//         auto digits = int_to_pw(low);
+//         if (check_valid(digits)) {
+//             writeln(low);
+//             result ~= low;
+//         }
+//         low++;
+//     }
+//     return result;
+// }
 
 
 void main() {
@@ -94,8 +171,10 @@ void main() {
 
     // writeln(bounds[1] > bounds[0]);  // array comparisons work, great!
     //writeln(int_to_pw(172930), int_to_pw(683082));
-    writeln(naive_generate(bounds[0], bounds[1]).length);
+    //writeln(naive_generate(bounds[0], bounds[1]).length);
     //writeln(naive_generate(bounds_int[0], bounds_int[1]).length);
+    Password6[] res = naive_generate(bounds_int[0], bounds_int[1]);
+    writeln(res.length);
 }
 
 
